c1c440825ffb8ea8ca9a8583fcaf262b7d575945
Mohd <junedgreat89@gmail.com>
2024-05-04 01:15:36 +0530

    Update the state

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -16,4 +16,4 @@ export const TodoContext = createContext({
 export const useTodo = () => {
     return useContext(TodoContext);
 }
-export const usetodoProvider = TodoContext.Provider;
+export const TodoProvider = TodoContext.Provider;
