a9207a6403da71dc85b359e660e69718651140ed
Mohd <junedgreat89@gmail.com>
2024-05-04 01:49:08 +0530

    Update the Change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { TodoProvider } from './context/TodoContext';
+import Todoform from './components/Todoform';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  
+
   // create 
-  const addTodo = (todo) =>{
-    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
   }
 
   const updateTodo = (id, todo) => {
@@ -17,28 +18,30 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((Todo) => Todo.id !== id))
   }
-  
+
   // istoggle complete
 
-  const isToggleComplete = (id) => {
-    setTodos((prev) => prev.map((todo) => todo.id === id ? {...todo, complete: !todo.complete} : todo
-  ))
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((todo) => todo.id === id ? { ...todo, complete: !todo.complete } : todo
+    ))
   }
 
-  
+
   return (
-    <TodoProvider value = {{todos, addTodo, deleteTodo, updateTodo, isToggleComplete}}>
-       <div className='min-h-screen py-8 bg-[#172842]' >
-      <div className='w-full max-w-2xl px-4 py-3 shadow-md mx-auto rounded-lg text-white'>
-        <h1 className='text-2xl text-center font-bold mb-5 mt-2'>Todo Management</h1>
-      <div className=''> 
-      </div>
-
-      </div>
-      
-    </div>
-
-    </TodoProvider>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+              <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                  <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                  <div className="mb-4">
+                      {/* Todo form goes here */} 
+                      <Todoform />
+                  </div>
+                  <div className="flex flex-wrap gap-y-3">
+                    
+                  </div>
+              </div>
+          </div>
+  </TodoProvider>
    
   )
 }
